ed932b9454392f2b8c2a0285b8c327bd474c4aa7
vanduy2003 <duy9541@gmail.com>
2024-07-08 23:24:20 +0700

    Add TestCase UpdateProduct, UpdateUser, AddUser

--- home/tests.py
+++ home/tests.py
@@ -178,6 +178,215 @@ class AddUserSystemTestCase(LiveServerTestCase):
             EC.presence_of_element_located((By.CLASS_NAME, 'alert-success'))
         )
 
+class UpdateProductSystemTestCase(LiveServerTestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.selenium = webdriver.Chrome()  # Khởi tạo WebDriver
+        cls.selenium.implicitly_wait(3)  # Đợi tối đa 10 giây cho các phần tử hiển thị
+        cls.port = 8000  # Đặt cổng
+        cls.live_server_url = f'http://localhost:{cls.port}'
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()  # Đóng WebDriver khi kết thúc
+        super().tearDownClass()
+
+    def test_update_product(self):
+        self.selenium.get('%s%s' % (self.live_server_url, '/login/'))
+
+        # Đợi cho trường email xuất hiện và điền thông tin
+        email_input = WebDriverWait(self.selenium, 3).until(
+            EC.presence_of_element_located((By.NAME, 'email'))
+        )
+        email_input.send_keys('admin@gmail.com')
+
+        # Đợi cho trường password xuất hiện và điền thông tin
+        password_input = WebDriverWait(self.selenium, 3).until(
+            EC.presence_of_element_located((By.NAME, 'password'))
+        )
+        password_input.send_keys('123')
+
+        # Submit form bằng cách nhấn Enter
+        password_input.send_keys(Keys.RETURN)
+
+        # Đợi cho trang admin xuất hiện
+        WebDriverWait(self.selenium, 3).until(
+            EC.presence_of_element_located((By.CLASS_NAME, 'navbar'))
+        )
+
+        # Chuyển đến trang quản lý sản phẩm
+        self.selenium.get('%s%s' % (self.live_server_url, '/manage-product/'))
+
+        # Đợi cho trang quản lý sản phẩm xuất hiện
+        WebDriverWait(self.selenium, 3).until(
+            EC.presence_of_element_located((By.CLASS_NAME, 'container'))
+        )
+
+        # Chọn sản phẩm cần sửa
+        product = WebDriverWait(self.selenium, 3).until(
+            EC.presence_of_element_located((By.CLASS_NAME, 'btn-warning'))
+        )
+        product.click()
+
+        # Đợi cho trang sửa sản phẩm xuất hiện
+        WebDriverWait(self.selenium, 3).until(
+            EC.presence_of_element_located((By.CLASS_NAME, 'container'))
+        )
+
+        # Đợi cho trường tên sản phẩm xuất hiện và điền thông tin
+        name_input = WebDriverWait(self.selenium, 3).until(
+            EC.presence_of_element_located((By.NAME, 'name'))
+        )
+        name_input.clear()
+        name_input.send_keys('Quả Lê')
+
+        # Đợi cho trường giá xuất hiện và điền thông tin
+        price_input = WebDriverWait(self.selenium, 3).until(
+            EC.presence_of_element_located((By.NAME, 'price'))
+        )
+        price_input.clear()
+        price_input.send_keys('20000')
+
+        # Đợi cho trường số lượng xuất hiện và điền thông tin
+        quantity_input = WebDriverWait(self.selenium, 3).until(
+            EC.presence_of_element_located((By.NAME, 'quantity'))
+        )
+        quantity_input.clear()
+        quantity_input.send_keys('200')
+
+        # Đợi cho trường mô tả xuất hiện và điền thông tin
+        description_input = WebDriverWait(self.selenium, 3).until(
+            EC.presence_of_element_located((By.NAME, 'desc'))
+        )
+        description_input.clear()
+        description_input.send_keys('Quả Lê ngon tuyệt vời')
+
+        # Đợi cho trường ảnh xuất hiện và điền thông tin
+        image_input = WebDriverWait(self.selenium, 3).until(
+            EC.presence_of_element_located((By.NAME, 'url'))
+        )
+        image_input.clear()
+        image_input.send_keys('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRy3zayBOjaN2QxHblUVyWB6sly_Se7AK6yYzT9Ns62Pqrh9Qgm-CxqTImkx7ECays-zA&usqp=CAU')
+
+        # Submit form bằng cách nhấn Enter
+        image_input.send_keys(Keys.RETURN)
+
+        # Đợi cho thông báo sửa sản phẩm thành công xuất hiện
+        WebDriverWait(self.selenium, 5).until(
+            EC.presence_of_element_located((By.CLASS_NAME, 'alert-success'))
+        )
+
+class UpdateUserSystemTestCase(LiveServerTestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.selenium = webdriver.Chrome()  # Khởi tạo WebDriver
+        cls.selenium.implicitly_wait(3)  # Đợi tối đa 10 giây cho các phần tử hiển thị
+        cls.port = 8000  # Đặt cổng
+        cls.live_server_url = f'http://localhost:{cls.port}'
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()  # Đóng WebDriver khi kết thúc
+        super().tearDownClass()
+
+    def test_update_user(self):
+        self.selenium.get('%s%s' % (self.live_server_url, '/login/'))
+
+        # Đợi cho trường email xuất hiện và điền thông tin
+        email_input = WebDriverWait(self.selenium, 3).until(
+            EC.presence_of_element_located((By.NAME, 'email'))
+        )
+        email_input.send_keys('admin@gmail.com')
+
+        # Đợi cho trường password xuất hiện và điền thông tin
+        password_input = WebDriverWait(self.selenium, 3).until(
+            EC.presence_of_element_located((By.NAME, 'password'))
+        )
+        password_input.send_keys('123')
+
+        # Submit form bằng cách nhấn Enter
+        password_input.send_keys(Keys.RETURN)
+
+        # Đợi cho trang admin xuất hiện
+        WebDriverWait(self.selenium, 3).until(
+            EC.presence_of_element_located((By.CLASS_NAME, 'navbar'))
+        )
+
+        # Chuyển đến trang quản lý người dùng
+        self.selenium.get('%s%s' % (self.live_server_url, '/manage-user/'))
+
+        # Đợi cho trang quản lý người dùng xuất hiện
+        WebDriverWait(self.selenium, 3).until(
+            EC.presence_of_element_located((By.CLASS_NAME, 'container'))
+        )
+
+        # Chọn người dùng cần sửa
+        user = WebDriverWait(self.selenium, 3).until(
+            EC.presence_of_element_located((By.CLASS_NAME, 'btn-warning'))
+        )
+        user.click()
+
+        # Đợi cho trang sửa người dùng xuất hiện
+        WebDriverWait(self.selenium, 3).until(
+            EC.presence_of_element_located((By.CLASS_NAME, 'container'))
+        )
+
+        # Đợi cho trường tên người dùng xuất hiện và điền thông tin
+        name_input = WebDriverWait(self.selenium, 3).until(
+            EC.presence_of_element_located((By.NAME, 'name'))
+        )
+        name_input.clear()
+        name_input.send_keys('Nguyễn Văn B')
+
+        # Đợi cho trường Ngày sinh xuất hiện và điền thông tin
+        dob_input = WebDriverWait(self.selenium, 3).until(
+            EC.presence_of_element_located((By.NAME, 'date'))
+        )
+        dob_input.clear()
+        dob_input.send_keys('01/01/2001')
+
+        # Đợi cho trường so dien thoai xuất hiện và điền thông tin
+        phone_input = WebDriverWait(self.selenium, 3).until(
+            EC.presence_of_element_located((By.NAME, 'phone'))
+        )
+        phone_input.clear()
+        phone_input.send_keys('0123456788')
+
+        # Đợi cho trường email xuất hiện và điền thông tin
+        email_input = WebDriverWait(self.selenium, 3).until(
+            EC.presence_of_element_located((By.NAME, 'email'))
+        )
+        email_input.clear()
+        email_input.send_keys('abc@gmail.com')
+
+        # Đợi cho trường password xuất hiện và điền thông tin
+        password_input = WebDriverWait(self.selenium, 3).until(
+            EC.presence_of_element_located((By.NAME, 'password'))
+        )
+        password_input.clear()
+        password_input.send_keys('123')
+
+        # Đợi cho trường Dịa chỉ xuất hiện và điền thông tin
+        address_input = WebDriverWait(self.selenium, 3).until(
+            EC.presence_of_element_located((By.NAME, 'address'))
+        )
+        address_input.clear()
+        address_input.send_keys('Hà Nội')
+
+        # Submit form bằng cách nhấn Enter
+        address_input.send_keys(Keys.RETURN)
+
+        # Đợi cho thông báo sửa người dùng thành công xuất hiện
+        WebDriverWait(self.selenium, 5).until(
+            EC.presence_of_element_located((By.CLASS_NAME, 'alert-success'))
+        )
+
+
+
+
+
 
 
 
